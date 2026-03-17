1e20b2973064e658e62c42b569f6ed1fa387c9c1
WisnuXiaomi <remensupriatin69@gmail.com>
2026-03-18 03:19:42 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam_2417051035/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051035/MainActivity.kt
@@ -5,22 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -36,6 +48,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrakTAM2_2417051035Theme {
+                Box(modifier = Modifier.fillMaxSize()){
+                    Image(
+                        painter = painterResource(id = R.drawable.bg),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
                 DaftarKegiatanScreen()
             }
         }
@@ -58,33 +78,50 @@ fun DaftarKegiatanScreen(){
 
 @Composable
 fun DetailScreen(activity: Activity){
-    Column(modifier = Modifier.fillMaxSize())
-    {
-        Image(
-            painter = painterResource(id = activity.imageRes),
-            contentDescription = activity.nama,
-            modifier = Modifier
-                .fillMaxSize()
-                .height(200.dp),
-            contentScale = ContentScale.Crop
-        )
+    var isFavorite by remember { mutableStateOf(false) }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box {
+            Image(
+                painter = painterResource(id = activity.imageRes),
+                contentDescription = activity.nama,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop
+            )
 
+            IconButton(
+                onClick = { isFavorite = !isFavorite },
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                    contentDescription = "Favorite Icon",
+                    tint = if (isFavorite) Color.Red else Color.Black
+                )
+            }
+        }
         Spacer(modifier = Modifier.height(16.dp))
 
         Text(
             text = activity.nama,
             style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            color = Color.White
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = activity.deskripsi,
             style = MaterialTheme.typography.headlineLarge,
+            color = Color.White
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = activity.nama,
             style = MaterialTheme.typography.headlineLarge,
+            color = Color.White
         )
         Spacer(modifier = Modifier.height(16.dp))
 
@@ -92,7 +129,9 @@ fun DetailScreen(activity: Activity){
             onClick = { },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text("Pilih Aktivitas")
+            Text(
+                text = "Pilih Aktivitas",
+                color = Color.White)
         }
     }
 
