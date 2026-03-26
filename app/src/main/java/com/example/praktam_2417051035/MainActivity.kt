f88776fc9367f34324157c93041869ea36ec025c
WisnuXiaomi <remensupriatin69@gmail.com>
2026-03-26 20:56:28 +0700

    Tugas LKP 5

--- app/src/main/java/com/example/praktam_2417051035/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051035/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,8 +17,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -29,15 +32,26 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praktam_2417051035.model.ActivitySource
 import com.example.praktam_2417051035.ui.theme.PrakTAM2_2417051035Theme
 import model.Activity
@@ -56,6 +70,7 @@ class MainActivity : ComponentActivity() {
                         contentScale = ContentScale.Crop
                     )
                 }
+//                HomeScreen()
                 DaftarKegiatanScreen()
             }
         }
@@ -63,15 +78,145 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun HomeScreen(){
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+        .padding(top = 50.dp, start = 20.dp, end = 20.dp)) {
+        Levelling(modifier =Modifier
+            .align(Alignment.TopStart))
+        Coin(modifier = Modifier
+            .align(Alignment.TopEnd))
+    }
+}
+
+@Composable
+fun Levelling(modifier: Modifier = Modifier){
+    val levelling = painterResource(R.drawable.levelling)
+    val Poppins = FontFamily(
+        Font(R.font.poppins_bold)
+    )
+    Box(modifier = Modifier
+        .width(100.dp)
+        .height(100.dp)
+    ) {
+        Image(
+            painter = levelling,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+
+        Text(
+            text = "Level 1",
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontFamily = Poppins,
+            fontSize = 15.sp,
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .padding(end = 12.dp, bottom = 3.dp)
+        )
+    }
+}
+
+@Composable
+fun Coin(modifier: Modifier = Modifier){
+    val levelling = painterResource(R.drawable.levelling)
+    Box(modifier = Modifier
+        .width(400.dp)
+        .height(100.dp)
+        .padding(start = 260.dp)
+    ) {
+        Image(
+            painter = levelling,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
+    }
+}
+
+@Composable
 fun DaftarKegiatanScreen(){
-    Column(modifier = Modifier
+//    Column(modifier = Modifier
+//        .fillMaxSize()
+//        .statusBarsPadding()
+//        .verticalScroll(rememberScrollState())
+//        .padding( 24.dp)
+//    ) {
+//        ActivitySource.dummyData.forEach{ activity ->
+//            DetailScreen(activity = activity)
+//            Spacer(modifier = Modifier.height(24.dp))
+//        }
+//    }
+
+    LazyColumn(modifier = Modifier
         .fillMaxSize()
-        .verticalScroll(rememberScrollState())
-        .padding(24.dp))
-    {
-        ActivitySource.dummyData.forEach { activity ->
+        .statusBarsPadding(),
+        contentPadding = PaddingValues(24.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        item {
+            Text(
+                text = "Rekomendasi Populer",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(ActivitySource.dummyData){ activity ->
+                    ActivityRowItem(activity = activity)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(45.dp))
+
+            Text(
+                text = "Daftar Kegiatan",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+        }
+
+        items(ActivitySource.dummyData){ activity ->
             DetailScreen(activity = activity)
-            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+fun ActivityRowItem(activity: Activity) {
+    Card(
+        modifier = Modifier.width(160.dp),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column {
+            Image(
+                painter = painterResource(activity.imageRes),
+                contentDescription = activity.nama,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(100.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = activity.nama,
+                    style = MaterialTheme.typography.titleSmall,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Deskripsi : ${activity.deskripsi}",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
         }
     }
 }
@@ -79,70 +224,87 @@ fun DaftarKegiatanScreen(){
 @Composable
 fun DetailScreen(activity: Activity){
     var isFavorite by remember { mutableStateOf(false) }
-    Column(modifier = Modifier.fillMaxSize()) {
-        Box {
-            Image(
-                painter = painterResource(id = activity.imageRes),
-                contentDescription = activity.nama,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .height(200.dp),
-                contentScale = ContentScale.Crop
-            )
+    Card(modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(6.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.onBackground
+        )
+    ) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Box {
+                Image(
+                    painter = painterResource(id = activity.imageRes),
+                    contentDescription = activity.nama,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(8.dp)
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Favorite Icon",
+                        tint = if (isFavorite) Color.Red else Color.Black
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
 
-            IconButton(
-                onClick = { isFavorite = !isFavorite },
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(8.dp)
+            Text(
+                text = activity.nama,
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = activity.deskripsi,
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = activity.nama,
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Icon(
-                    imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                    contentDescription = "Favorite Icon",
-                    tint = if (isFavorite) Color.Red else Color.Black
+                Text(
+                    text = "Pilih Aktivitas",
+                    color = Color.White
                 )
             }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = activity.nama,
-            style = MaterialTheme.typography.headlineMedium,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = activity.deskripsi,
-            style = MaterialTheme.typography.headlineLarge,
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = activity.nama,
-            style = MaterialTheme.typography.headlineLarge,
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = { },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = "Pilih Aktivitas",
-                color = Color.White)
-        }
     }
-
 }
 
-@Preview(showBackground = true, showSystemUi = true,)
-@Composable
-fun GreetingPreview() {
-    PrakTAM2_2417051035Theme {
+    @Preview(showBackground = true, showSystemUi = true,)
+    @Composable
+    fun GreetingPreview() {
         PrakTAM2_2417051035Theme {
-            DaftarKegiatanScreen()
+            PrakTAM2_2417051035Theme {
+                Box(modifier = Modifier.fillMaxSize()){
+                    Image(
+                        painter = painterResource(id = R.drawable.bg),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+
+                }
+//                  HomeScreen()
+                    DaftarKegiatanScreen()
+            }
         }
     }
-}
